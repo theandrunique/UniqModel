929ff8f9ec37d8d0e272dc74b59792abcc8da5d7
theandrunique <theandrunique@gmail.com>
2024-01-21 14:29:15 +0300

    changed CI to push if the version was changed

--- SQLModel/TableBuilder.cs
+++ SQLModel/TableBuilder.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Reflection;
-using System.Windows.Documents;
 
 namespace SQLModel
 {
